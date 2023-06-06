cab8bc1aa5196e5173592178afc2c905a3373184
Bijay Gautam <bijay.gautam1501@gmail.com>
2023-06-06 23:09:50 +0545

    BACKEND CONFIGURATIONS

--- StockPricePrediction/urls.py
+++ StockPricePrediction/urls.py
@@ -1,7 +1,7 @@
 """StockPricePrediction URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.urls import path, include
+from django.conf.urls.static import static
+from StockPricePrediction import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('',include('StockPricePredictionApp.urls'))
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
