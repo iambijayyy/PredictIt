48a3878db2af9077295602b2d9284bb7e5d2fc34
Bijay Gautam <bijay.gautam1501@gmail.com>
2023-06-25 21:24:40 +0545

    STOCKS DETAILS FETCH FROM YFINANCE

--- StockPricePredictionApp/views.py
+++ StockPricePredictionApp/views.py
@@ -375,3 +375,45 @@ def all_stocks(request):
         "flag":True
     }
     return render(request,'all_stocks.html',context) 
+
+def details(request,id):
+    global df
+    stocks = str(id)
+
+    apple = yf.Ticker(stocks)
+    des=apple.info
+    temp_des={}
+    for key in des:
+        if des[key]!='None' and des[key]!=[]:
+            temp_des[key]=des[key]
+    des=temp_des
+    df=apple.history(start='2000-01-01', end='2032-05-13',  actions=False)
+    df['Date']=df.index.strftime('%d-%m-%Y')
+    x=list(map(str,df.index.strftime('%d-%m-%y')))
+    y_high=list(df['High'])
+    y_open=list(df['Open'])
+    y_low=list(df['Low'])
+    y_close=list(df['Close'])
+    y_volume=list(df['Volume'])    
+    context={
+            'x':x,
+            'y_high':y_high,
+            'y_low':y_low,
+            'y_open':y_open,
+            'y_close':y_close,
+            'y_volume':y_volume,
+            'company':stocks,
+            'df':df,
+            'predicted_x':[1,2,3,4,5],
+            'predicted_y':[5,4,3,2,1],
+            'max_price':round(max(y_high),2),
+            'min_price':round(min(y_low),2),
+            'last_day_price':round(y_close[-1],2),
+            'change_in_price':round(y_high[-1]-y_high[0],2),
+            'change_in_precentage':round(((y_high[-1]-y_high[0])/y_high[0])*100,2),
+            "description":des,
+            "flag":True,
+            'company':stocks,
+        }
+    
+    return render(request,'details.html',context)
